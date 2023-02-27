208f01403e81b4f49fd71c56005059eeca26900f
nao <iromnao60@gmail.com>
2023-02-27 13:49:13 +0530

    Navbar Completed

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,3 +1,5 @@
+import { Badge } from '@material-ui/core';
+import { Search, ShoppingCartOutlined } from '@material-ui/icons';
 import React from 'react'
 import styled from 'styled-components'
 
@@ -8,18 +10,68 @@ const Container = styled.div`
 const Wrapper = styled.div`
   padding: 10px 20px;
   display: flex;
+  align-items: center;
   justify-content: space-between;
 `
-const left = styled.div``
-const center = styled.div``
-const right = styled.div``
+const Left = styled.div`flex: 1;
+  display: flex;
+  align-items: center;`;
+
+const Language = styled.span`
+  font-size: 14px;
+  cursor: pointer;`;
+
+const SearchContainer = styled.div`
+  border:0.5px solid lightgray;
+  display: flex;
+  align-items: center;
+  margin-left: 25px;
+  padding: 5px;
+`;
+
+const Input = styled.input`
+  border: none;
+`
+
+const Center = styled.div`flex: 1;
+  text-align: center; `;
+const Logo = styled.h1`
+  font-weight: bold;`;
+
+const Right = styled.div`flex: 1; 
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;`;
+
+const MenuItem = styled.div`
+  font-size: 14px;
+  cursor: pointer;
+  margin-left: 25px;
+`;
+
 const Navbar = () => {
   return (
     <Container>
       <Wrapper>
-        <left>left</left>
-        <center>center</center>
-        <right>right</right>
+        <Left>
+          <Language>EN</Language>
+          <SearchContainer>
+            <Input></Input>
+            <Search style={{ color: "gray", fontSize: 16 }} />
+          </SearchContainer>
+        </Left>
+        <Center><Logo>HAREKMAAL.</Logo></Center>
+        <Right>
+          <MenuItem>REGISTER</MenuItem>
+          <MenuItem>SIGN IN</MenuItem>
+          <MenuItem>
+            <Badge badgeContent={4} color="primary">
+              <ShoppingCartOutlined
+                color="action" />
+            </Badge>
+          </MenuItem>
+
+        </Right>
       </Wrapper>
     </Container>
   )
