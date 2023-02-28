4b8bc00f3924ec9b2bb9aaf902641fad1a373e32
nao <iromnao60@gmail.com>
2023-03-01 01:58:08 +0530

    Slider Completed

---
+++ src/data.js
@@ -0,0 +1,23 @@
+export const slideritems = [
+	{
+		id: 1,
+		img: "https://i.ibb.co/0s6Xxfj/Women-Model-Transparent-File.png",
+		title: "SUMMER SALE",
+		desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+		bg: "f5fafd",
+	},
+	{
+		id: 2,
+		img: "https://i.ibb.co/0s6Xxfj/Women-Model-Transparent-File.png",
+		title: "AUTUMN COLLECTION",
+		desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+		bg: "fcf1ed",
+	},
+	{
+		id: 3,
+		img: "https://i.ibb.co/0s6Xxfj/Women-Model-Transparent-File.png",
+		title: "LOUNGEWEAR LOVE",
+		desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+		bg: "fbf0f4",
+	},
+];
