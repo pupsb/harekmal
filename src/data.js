cdaf15947d7c9a99d0f85912ebd0d187e7f237db
nao <iromnao60@gmail.com>
2023-03-03 01:27:40 +0530

    Product List UI Completed

--- src/data.js
+++ src/data.js
@@ -39,3 +39,38 @@ export const categories = [
 		title: "LIGHT JACKETS",
 	},
 ];
+
+export const popularProducts = [
+	{
+		id: 1,
+		img: "https://lp2.hm.com/hmgoepprod?set=source[/dc/77/dc77bcec6f1bd9d93e3236a172efa05d189a7149.jpg],origin[dam],category[],type[DESCRIPTIVESTILLLIFE],res[y],hmver[2]&call=url[file:/product/main]",
+	},
+	{
+		id: 2,
+		img: "https://lp2.hm.com/hmgoepprod?set=source[/32/49/3249c2441d3b7b5f54ed5a1c336e2a150bc3765e.jpg],origin[dam],category[ladies_basics_tops_shortsleeve],type[DESCRIPTIVESTILLLIFE],res[y],hmver[2]&call=url[file:/product/main]",
+	},
+	{
+		id: 3,
+		img: "https://lp2.hm.com/hmgoepprod?set=source[/6f/c5/6fc57f7ba6f2bfc496e1ee36030498b76a9d4f57.jpg],origin[dam],category[],type[DESCRIPTIVESTILLLIFE],res[y],hmver[2]&call=url[file:/product/main]",
+	},
+	{
+		id: 4,
+		img: "https://lp2.hm.com/hmgoepprod?set=source[/a2/60/a26062ea2bd433effeacbf0b53feae5acd4bb633.jpg],origin[dam],category[],type[DESCRIPTIVESTILLLIFE],res[y],hmver[2]&call=url[file:/product/main]",
+	},
+	{
+		id: 5,
+		img: "https://lp2.hm.com/hmgoepprod?set=source[/50/25/5025ce3378c7d64bf62f3bb122fc65041389d111.jpg],origin[dam],category[ladies_dresses_mididresses],type[DESCRIPTIVESTILLLIFE],res[y],hmver[2]&call=url[file:/product/main]",
+	},
+	{
+		id: 6,
+		img: "https://lp2.hm.com/hmgoepprod?set=source[/8d/2a/8d2a2a8b5fef5384decc6de2f9facf5530c56a38.jpg],origin[dam],category[ladies_dresses_cutout],type[DESCRIPTIVESTILLLIFE],res[y],hmver[2]&call=url[file:/product/main]",
+	},
+	{
+		id: 7,
+		img: "https://lp2.hm.com/hmgoepprod?set=source[/0b/62/0b62202e3ba9f79eb91c88d8625fe6eecb61194b.jpg],origin[dam],category[ladies_blazers],type[DESCRIPTIVESTILLLIFE],res[y],hmver[2]&call=url[file:/product/main]",
+	},
+	{
+		id: 8,
+		img: "https://lp2.hm.com/hmgoepprod?set=source[/72/f4/72f4c4c8e7a6a61ab678b8cfd53420efe0342080.jpg],origin[dam],category[],type[DESCRIPTIVESTILLLIFE],res[y],hmver[2]&call=url[file:/product/main]",
+	},
+];
