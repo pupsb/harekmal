421ea780321b3aec9c4a9e9d250272d6039defe4
nao <iromnao60@gmail.com>
2023-03-06 16:08:54 +0530

    Fotter UI Completed

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,126 @@
+import styled from "styled-components"
+import { Facebook, Instagram, MailOutline, Phone, Pinterest, Room, Twitter } from "@material-ui/icons"
+
+
+const Container = styled.div`
+  display: flex;`
+const Left = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  padding: 20px;`
+const Logo = styled.h1`
+  font-weight: bold;
+  font-size: 20px;
+  color: teal;
+  `
+const Desc = styled.p`
+  margin: 20px 0px;
+  `
+const SocialContainer = styled.div`
+  display: flex;
+  `
+
+const SocialIcon = styled.div`
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  background-color: #${props => props.color};
+  color: white;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-right: 20px;`
+
+
+const Center = styled.div`
+  flex: 1;
+  padding: 20px;
+  `
+const Title = styled.h3`
+  margin-bottom: 30px;
+  `
+const List = styled.ul`
+  margin: 0;
+  padding: 0;
+  list-style: none;
+  display: flex;
+  flex-wrap: wrap;
+  `
+const ListItem = styled.li`
+  width: 50%;
+  margin-bottom: 10px;
+  `
+
+const Right = styled.div`
+flex: 1;
+padding: 20px; `
+
+const ContactItem = styled.div`
+  margin-bottom: 20px;
+  display: flex;
+  align-items: center;
+  `
+const Payment = styled.img`
+  width: 50%;
+  `
+
+
+const Footer = () => {
+  return (
+    <Container>
+      <Left>
+        <Logo>HAREKMAAL.</Logo>
+        <Desc>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Quisquam, quod.
+        </Desc>
+        <SocialContainer>
+          <SocialIcon color="3B5999">
+            <Facebook />
+          </SocialIcon>
+          <SocialIcon color="E4405F">
+            <Instagram />
+          </SocialIcon>
+          <SocialIcon color="55ACEE">
+            <Twitter />
+          </SocialIcon>
+          <SocialIcon color="E60023">
+            <Pinterest />
+          </SocialIcon>
+        </SocialContainer>
+      </Left>
+      <Center>
+        <Title>Useful Links</Title>
+        <List>
+          <ListItem>Home</ListItem>
+          <ListItem>Cart</ListItem>
+          <ListItem>Men Fashion</ListItem>
+          <ListItem>Women Fashion </ListItem>
+          <ListItem>Accessories</ListItem>
+          <ListItem>Order Tracking</ListItem>
+          <ListItem>Wishlist</ListItem>
+          <ListItem>Terms&Conditions</ListItem>
+        </List>
+      </Center>
+      <Right>
+        <Title>Contact</Title>
+        <ContactItem>
+          <Room style={{ marginRight: "10px" }} />
+          <b>Address:</b> 781022 Guwahati, India
+        </ContactItem>
+        <ContactItem>
+          <Phone style={{ marginRight: "10px" }} />
+          <b>Phone:</b> +91 9395578107
+        </ContactItem>
+        <ContactItem>
+          <MailOutline style={{ marginRight: "10px" }} />
+          <b>Email:</b> iromnao60@gmail.com
+        </ContactItem>
+        <Payment src="https://clipground.com/images/visa-mastercard-american-express-png-1.png" />
+      </Right>
+    </Container>
+  )
+}
+
+export default Footer
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,6 +2,7 @@
 import React from 'react'
 import Announcement from '../Components/Announcement'
 import Categories from '../Components/Categories'
+import Footer from '../Components/Footer'
 import Navbar from '../Components/Navbar'
 import Newsletter from '../Components/Newsletter'
 import Products from '../Components/Products'
@@ -16,6 +17,7 @@ const Home = () => {
       <Categories />
       <Products />
       <Newsletter />
+      <Footer />
     </div>
   )
 }
