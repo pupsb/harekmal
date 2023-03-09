79ade02a49a7a4e340b430fdce5baebf488a59e4
nao <iromnao60@gmail.com>
2023-03-10 00:25:49 +0530

    Register and Login UI conpleted

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Home from "./Pages/Home";
+import Login from "./Pages/Login";
 import Product from "./Pages/Product";
 import ProductList from "./Pages/ProductList";
+import Register from "./Pages/Register";
 
 const App = () => {
-  return <Product />;
+  return <Login />;
 };
 
 export default App;
